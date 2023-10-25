c26f454809fb93c680d6e6a101b6c29983b3ecd1
Matt Armstrong <matt@rfc20.org>
2023-10-25 06:43:26 -0700

    Add button to change the board dimensions

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 let dimension = 16;
 
+const changeDimensions = () => {
+    dimension = +prompt("Enter a number between 1 and 100", 16);
+    if (isNaN(dimension) || dimension < 1 || dimension > 100) {
+        dimension = 16;
+    }
+    remakeGrid();
+}
+
 const remakeGrid = () => {
     const board = document.querySelector("#board");
     while (board.firstChild) {
@@ -23,4 +31,9 @@ const onMouseEnter = (e) => {
     cell.classList.add("hovered");
 }
 
+
+{
+    const button = document.querySelector("#change-dimensions");
+    button.addEventListener("click", changeDimensions);
+}
 remakeGrid();
